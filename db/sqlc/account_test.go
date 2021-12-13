018242ea9eed0692e94c591a1683e0166c9f9f80
Dimitrii Vinogradov <siteddv@gmail.com>
2021-12-14 01:57:23 +0600

    added tests for accounts CRUD

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,18 +5,16 @@ import (
 	"database/sql"
 	"github.com/siteddv/simple-bank/util"
 	"github.com/stretchr/testify/require"
-	"log"
 	"testing"
+	"time"
 )
 
-func Test_CreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
-
-	testQueries := GetTestQueries()
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 
@@ -30,19 +28,79 @@ func Test_CreateAccount(t *testing.T) {
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
-	/*err = testQueries.DeleteAccount(context.Background(), account.ID)
-	if err != nil {
-		log.Fatal("error handled during deleting account: ", err)
-	}*/
+	return account
 }
 
-func GetTestQueries() *Queries {
-	conn, err := sql.Open(dbDriver, dbSource)
-	if err != nil {
-		log.Fatal("cannot connect to db: ", err)
+func Test_CreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func Test_GetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func Test_UpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
 	}
 
-	testQueries := New(conn)
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
 
-	return testQueries
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.NotEqualValues(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func Test_DeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+func Test_ListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,10 +1,25 @@
 package db
 
 import (
+	"database/sql"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5436/simple_bank_db?sslmode=disable"
 )
+
+var testQueries = GetTestQueries()
+
+func GetTestQueries() *Queries {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	testQueries := New(conn)
+
+	return testQueries
+}
