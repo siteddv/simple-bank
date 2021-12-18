ff6b53d786ae2a7cfd3c99d2e3c37709e1395ccd
Dimitrii Vinogradov <siteddv@gmail.com>
2021-12-18 23:12:28 +0600

    added tests for db deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -62,51 +62,64 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 	var result TransferTxResult
 
 	err := store.execTr(ctx, func(q *Queries) error {
-		var execError error
+		var err error
 
-		createTransferArgs := CreateTransferParams{
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
 			ToAccountID:   arg.ToAccountID,
 			Amount:        arg.Amount,
-		}
-		result.Transfer, execError = q.CreateTransfer(ctx, createTransferArgs)
-		if execError != nil {
-			return execError
+		})
+		if err != nil {
+			return err
 		}
 
-		createEntryArgs := CreateEntryParams{
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
 			Amount:    -arg.Amount,
-		}
-		result.FromEntry, execError = q.CreateEntry(ctx, createEntryArgs)
-		if execError != nil {
-			return execError
+		})
+		if err != nil {
+			return err
 		}
 
-		createEntryArgs = CreateEntryParams{
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
-		}
-		result.ToEntry, execError = q.CreateEntry(ctx, createEntryArgs)
-		if execError != nil {
-			return execError
-		}
-
-		result.FromAccount, execError = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
 		})
-		if execError != nil {
-			return execError
+		if err != nil {
+			return err
 		}
 
-		result.ToAccount, execError = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
 
-		return execError
+		return err
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
 }
